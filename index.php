8de2b15666faa280fc3e487ce520df2bf92778a7
pranavmalwatkar <130284009+pranavmalwatkar@users.noreply.github.com>
2025-06-15 13:03:47 +0530

    change

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <link rel="stylesheet" href="../AdvenTour/index.css">
   <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
 function clearContactInfo() {
     // Clear the text inside the <ul> with id "contact-info"
